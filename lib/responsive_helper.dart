df3d561d7eaf142cc75e07edf713474f04b09f97
Linda Smith <linda.anne.onyejiaka@gmail.com>
2022-04-07 04:25:42 -0400

    Updated ResponsiveHelper for Testing

--- lib/responsive_helper.dart
+++ lib/responsive_helper.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 enum ScreenType {
+    None,
     Watch,
     SmallPhone,
     MediumPhone,
@@ -17,17 +18,19 @@ enum ScreenType {
 
 class ResponsiveHelper {
 
-  static late ResponsiveHelper _instance;
-  static ResponsiveHelper get instance { return _instance; }
+  static ResponsiveHelper? _instance;
+  static ResponsiveHelper? get instance { return _instance; }
   static void init(BuildContext context, BoxConstraints boxConstraints) {
       _instance = ResponsiveHelper(
         orientation: Orientation.portrait,
         screenSize: Size(boxConstraints.maxWidth, boxConstraints.maxHeight),
       );
-      _instance._setupScreenType();
+      _instance!._setupScreenType();
   }
 
   static late ScreenType _screenType;
+
+  static ScreenType testingScreenType = ScreenType.None;
 
   Orientation orientation;
   Size screenSize;
@@ -49,6 +52,16 @@ class ResponsiveHelper {
       return _screenType.index < _screenTypeOption.index;
   }
 
+  static get screenType {
+      return _screenType;
+  }
+
+  static void updateTestingScreenType() {
+    if(_instance != null) {
+        _instance!._setupScreenType();
+    }
+  }
+
   void _setupScreenType() {
 
       double deviceWidth = 0;
@@ -58,27 +71,38 @@ class ResponsiveHelper {
         deviceWidth = screenSize.width;
       }
 
-      if (deviceWidth >= 1600) { // 1920
-        _screenType = ScreenType.Television;
-      } else if (deviceWidth >= 1420) {
-        _screenType = ScreenType.LargeDesktop;
-      } else if (deviceWidth >= 1280) {
-        _screenType = ScreenType.MediumDesktop;
-      } else if (deviceWidth >= 1024) {
-        _screenType = ScreenType.LargeTablet;
-      } else if (deviceWidth >= 800) {
-        _screenType = ScreenType.MediumTablet;
-      } else if (deviceWidth >= 600) {
-        _screenType = ScreenType.SmallTablet;
-      } else if (deviceWidth >= 480) {
-        _screenType = ScreenType.LargePhone;
-      } else if (deviceWidth >= 360) {
-        _screenType = ScreenType.MediumPhone;
-      } else if (deviceWidth >= 320) {
-        _screenType = ScreenType.SmallPhone;
-      } else { _screenType = ScreenType.Watch; }
+      if(testingScreenType != ScreenType.None) {
+        _screenType = testingScreenType;
+      } else {
+        _screenType = setupScreenType(deviceWidth);
+      }
 
       if(!kReleaseMode) print('\n\n${toString()}\n\n');
+  }
+
+  static ScreenType setupScreenType(double deviceWidth) {
+
+      if (deviceWidth >= 1600) { // 1920
+        return ScreenType.Television;
+      } else if (deviceWidth >= 1420) {
+        return ScreenType.LargeDesktop;
+      } else if (deviceWidth >= 1280) {
+        return ScreenType.MediumDesktop;
+      } else if (deviceWidth >= 1024) {
+        return ScreenType.LargeTablet;
+      } else if (deviceWidth >= 800) {
+        return ScreenType.MediumTablet;
+      } else if (deviceWidth >= 600) {
+        return ScreenType.SmallTablet;
+      } else if (deviceWidth >= 480) {
+        return ScreenType.LargePhone;
+      } else if (deviceWidth >= 360) {
+        return ScreenType.MediumPhone;
+      } else if (deviceWidth >= 320) {
+        return ScreenType.SmallPhone;
+      } else if (deviceWidth >= 150) {
+        return ScreenType.Watch;
+      } else { return ScreenType.None; }
 
   }
 
